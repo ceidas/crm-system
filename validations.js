702b936aaba5aa299921e0d969142e04856aa675
ce <15091642+ceidas@users.noreply.github.com>
2021-12-17 02:03:56 +0000

    design

---
+++ validations.js
@@ -0,0 +1,41 @@
+// function validationForm(){
+//     let x = document.forms["myForm"].value;
+//     if ( x==""){
+//         alert(All cells must be filled out);
+//         return false;
+//     }
+// }
+//https://www.w3schools.com/js/js_validation.asp 
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_number
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_js
+
+
+// ************* COURSES ************* //
+
+function validateStudentForm(){
+    // let courseName = document.getElementById("coursename");
+    text = "Input OK";
+
+    document.getElementById("printError").innerHTML = text;
+    console.log("testttt")
+
+}
+
+
+
+// ************* ASSIGNMENTS ************* //
+
+
+// ************* ASSIGNMENTS/COURSE ************* //
+
+
+// ************* TRAINERS ************* //
+
+
+// ************* TRAINERS/COURSE ************* //
+
+
+// ************* STUDENTS ************* //
+
+
+// ************* STUDENTS/COURSE ************* //

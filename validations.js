667bbe3346a7a7b277d35967833a733729aa181f
ce <15091642+ceidas@users.noreply.github.com>
2021-12-18 03:48:18 +0000

    course form validations

--- validations.js
+++ validations.js
@@ -9,15 +9,57 @@
 //https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_number
 //https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_js
 
-
 // ************* COURSES ************* //
 
-function validateStudentForm(){
-    // let courseName = document.getElementById("coursename");
-    text = "Input OK";
+document.getElementById("courseForm").addEventListener('submit', validateCourseForm)
 
-    document.getElementById("printError").innerHTML = text;
-    console.log("testttt")
+function validateCourseForm(e) {
+
+    var coursename = document.getElementById("courseName");
+    var stream = document.getElementById("stream");
+    var type = document.getElementById("stream");
+    var startdate = document.getElementById("startDate");
+    var enddate = document.getElementById("endDate");
+
+    if (coursename.value === "" || stream.value === "" || type.value === "" || startdate.value === "" || enddate.value === "") {
+        document.getElementById("courseValidation").innerHTML = "Required Fields";
+        document.getElementById("courseForm").style.color = "red";
+        e.preventDefault();
+    } else {
+
+        document.getElementById("courseValidation").innerHTML = "";
+
+        var patternOne = /^CB/;
+        var patternTwo = /^De/;
+
+        if ((patternOne.test(coursename.value) !== true) && (patternTwo.test(coursename.value) !== true)) {
+            document.getElementById("courseNameValidation").innerHTML = "Course name must start with either CB or De";
+            e.preventDefault();
+        } else {
+            document.getElementById("courseNameValidation").innerHTML = "";
+        }
+        // let now = new Date();
+        // console.log(new Date())
+        let start = new Date(startdate.value);
+        let end = new Date(enddate.value);
+        // console.log(new Date(startdate.value));
+
+        if (end.getFullYear() - start.getFullYear() > 1) {
+            document.getElementById("startDateValidation").innerHTML = "Cannot be more than one year difference";
+            e.preventDefault();
+
+        } else if ((end.getFullYear() < start.getFullYear()) || ((end.getFullYear() === start.getFullYear()) && end.getMonth() < start.getMonth())) {
+            document.getElementById("startDateValidation").innerHTML = "Invalid date input";
+            e.preventDefault();
+        } else {
+            document.getElementById("startDateValidation").innerHTML = "";
+        }
+
+
+
+
+
+    }
 
 }
 
